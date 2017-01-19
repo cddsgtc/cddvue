b145fbde9dc0b734eef52151ed8139c4ab1fde1f
cddsgtc <cddsgtc@163.com>
2017-01-19 15:01:29 +0800

    初始化

--- client/index.js
+++ client/index.js
@@ -2,8 +2,4 @@ import './css/main.css';
 import "./promise.polyfill";
 import {a} from './test';
 console.log(a);
-console.log('cd');
-console.log('csdsdfafs ');
-console.log('cdd');
-console.log('cdd');
-console.log(Promise);
+
